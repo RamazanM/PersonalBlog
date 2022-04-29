12dc83ef0ac8466a0da0c8452db0e2f5c971061a
RamazanM <r.ramazanmutlu@gmail.com>
2022-04-30 02:45:01 +0300

    Removed Prime, decided to write from scratch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+var routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes), FormsModule],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
