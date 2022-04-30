2c4142265a2fbc245f5c058a6619476ad9cc5539
RamazanM <r.ramazanmutlu@gmail.com>
2022-04-30 23:44:14 +0300

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BlogComponent } from './pages/blog/blog.component';
 import { PhotosComponent } from './pages/photos/photos.component';
 import { CvComponent } from './pages/cv/cv.component';
@@ -31,12 +30,7 @@ var routes: Routes = [
 ];
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes),
-    FormsModule,
-    FontAwesomeModule,
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes), FormsModule],
   declarations: [AppComponent, HeaderComponent, HomeComponent],
   bootstrap: [AppComponent],
 })
