29b94207cf47977b93711c924fda5cfc6ca1799c
RamazanM <r.ramazanmutlu@gmail.com>
2022-05-17 23:35:40 +0300

    Admin dashboard files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { BlogComponent } from './pages/blog/blog.component';
 import { PhotosComponent } from './pages/photos/photos.component';
 import { CvComponent } from './pages/cv/cv.component';
+import { DashboardComponent } from './admin/pages/dashboard/dashboard.component';
 
 var routes: Routes = [
   {
@@ -26,6 +27,15 @@ var routes: Routes = [
   {
     path: 'cv',
     component: CvComponent,
+  },
+  {
+    path: 'admin',
+    children: [
+      {
+        path: '',
+        component: DashboardComponent,
+      },
+    ],
   },
 ];
 
