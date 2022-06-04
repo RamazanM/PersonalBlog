c124de15b6b024cda93c5e96c6fcfb8ce489e20c
RamazanM <r.ramazanmutlu@gmail.com>
2022-06-04 16:22:10 +0300

    convert to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,11 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   name = 'RamazanM';
-  constructor(){}
+  constructor() {}
 
-  ngOnInit(){
-  }
+  ngOnInit() {}
 }
-
