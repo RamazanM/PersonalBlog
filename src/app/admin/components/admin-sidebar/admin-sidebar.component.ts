7046378b6345a1b764891d95c70d66d3ab737cec
RamazanM <r.ramazanmutlu@gmail.com>
2022-06-05 14:38:12 +0300

    Admin page css edits

--- src/app/admin/components/admin-sidebar/admin-sidebar.component.ts
+++ src/app/admin/components/admin-sidebar/admin-sidebar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-admin-sidebar',
   templateUrl: './admin-sidebar.component.html',
-  styleUrls: ['./admin-sidebar.component.css']
+  styleUrls: ['./admin-sidebar.component.css'],
 })
 export class AdminSidebarComponent implements OnInit {
+  public hidden = true;
+  leftIcon = faChevronLeft;
+  rightIcon = faChevronRight;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  toggle() {
+    this.hidden = !this.hidden;
   }
-
 }
